09b0628835786683de3c396551f53fb18e10f86b
raphacosta27 <rapha2706@hotmail.com>
2017-05-29 12:40:00 -0300

    Friends List ready

--- app/src/main/java/pesadadobatata/songsync/FriendsActivity.java
+++ app/src/main/java/pesadadobatata/songsync/FriendsActivity.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
 
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,9 +24,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -32,16 +44,22 @@ public class FriendsActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
     private HashMap[] eita;
+    private List<Friend> userNames;
+    private String[] usernamesArray;
+    private GridView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mAuth =  FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+        userNames = new LinkedList<>();
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_friends);
+        list = (GridView) findViewById(R.id.friendsList);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -55,44 +73,56 @@ public class FriendsActivity extends AppCompatActivity {
         final ValueEventListener valueEventListener = mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-//                Map<String, String> value = dataSnapshot.getValue(Map.class);
-                Map<HashMap, HashMap> value = (Map<HashMap, HashMap>) dataSnapshot.getValue();
-                HashMap<HashMap, HashMap> users = new HashMap<>();
-                Log.d("Database now", "Value is: " + value);
-                for (Map.Entry<HashMap, HashMap> entry : value.entrySet()) {
-                    users.put(entry.getKey(), entry.getValue());
-                    System.out.printf("Key : %s and Value: %s %n", entry.getKey(), entry.getValue());
-                    Log.d("alo", entry.getClass().toString());
-//                    System.out.printf(entry.getKey().getClass().toString());
-//                    System.out.printf(String.valueOf()entry.getValue().getClass()));
+                for (DataSnapshot messageSnapshot : dataSnapshot.getChildren()) {
+                    String uid = (String) messageSnapshot.getKey();
+                    String userName = (String) messageSnapshot.child("Username").getValue();
+                    Log.d("name", uid);
+                    Log.d("message", userName);
+                    userNames.add(new Friend(uid, userName));
                 }
 
-//                Iterator<Map.Entry<, HashMap>>
-//                Iterator<Map.Entry<HashMap<String, String>, HashMap<String, String>>> parent = alo.entrySet().iterator();
-//                while (parent.hasNext()) {
-//                    Map.Entry<String, HashMap<String, String>> parentPair = parent.next();
-//                    System.out.println("parentPair.getKey() :   " + parentPair.getKey() + " parentPair.getValue()  :  " + parentPair.getValue());
-//
-////                    Iterator<Map.Entry<String, String>> child = (parentPair.getValue()).entrySet().iterator();
-////                    while (child.hasNext()) {
-////                        Map.Entry childPair = child.next();
-////                        System.out.println("childPair.getKey() :   " + childPair.getKey() + " childPair.getValue()  :  " + childPair.getValue());
-////
-////                        child.remove(); // avoids a ConcurrentModificationException
-////                    }
-//
-//                }
+                showUsers();
 
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
+            public void onCancelled(DatabaseError databaseError) {
                 // Failed to read value
-                Log.w("Erou", "Failed to read value.", error.toException());
+                Log.w("Erou", "Failed to read value.", databaseError.toException());
             }
         });
     }
 
+    public void showUsers(){
+        usernamesArray = new String[userNames.size()];
+        for (int i=0;i<usernamesArray.length;i++){
+            usernamesArray[i] = userNames.get(i).getUserName();
+        }
+
+        final List<String> alo = new ArrayList<String>(Arrays.asList(usernamesArray));
+        final ArrayAdapter<String> gridViewArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alo);
+        list.setAdapter(gridViewArrayAdapter);
+    }
+
 }
+
+
+
+
+
+
+
+
+class Friend{
+    private String UserID;
+    private String userName;
+
+    public Friend(String UserId, String userName){
+            this.UserID = UserID;
+            this.userName = userName;
+
+        }
+    public String getUserName(){
+        return this.userName;
+    }
+    }
